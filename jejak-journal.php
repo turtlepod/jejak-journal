ef3d7df3237c17f93241b4e21e86d23dedf8bb4a
David <david.warna@gmail.com>
2026-06-24 22:32:02 +0700

    chore: add License URI, update Author URI, add license.txt

--- jejak-journal.php
+++ jejak-journal.php
@@ -7,8 +7,9 @@
  * Requires at least: 6.0
  * Requires PHP: 8.2
  * Author: David Chandra Purnama
- * Author URI: https://pandaplugin.com/
+ * Author URI: https://turtlepod.xyz
  * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: jejak-journal
  * Domain Path: /languages/
  *
